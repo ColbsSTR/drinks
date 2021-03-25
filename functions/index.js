95b2de5bed164a6a5cb682f4ea56669d67a5a935
Luke Faupel <lukefaupel@gmail.com>
2021-03-25 10:33:29 -0500

    updated cloud function to use header and body of notif from firebase

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,6 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 exports.sendNotification = functions.firestore
     .document("Notifications/{notificationId}")
     .onWrite((change, context) => {
@@ -17,8 +9,8 @@ exports.sendNotification = functions.firestore
           .messaging()
           .sendToTopic("main", {
             notification: {
-              title: "test title",
-              body: "test body",
+              title: change.after.data().Header,
+              body: change.after.data().Body,
             },
           })
           .then((response) => {
@@ -28,4 +20,3 @@ exports.sendNotification = functions.firestore
             console.log("Error sending message:", error);
           });
     });
-
