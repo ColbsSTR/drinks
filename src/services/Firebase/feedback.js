3e06f033871875a24340d7325942c7898ad5331c
Colby Crowne <colbycrowne@Colbys-MBP.attlocal.net>
2021-05-22 20:55:18 -0500

    fixed feedback form and added some validation

--- src/services/Firebase/feedback.js
+++ src/services/Firebase/feedback.js
@@ -2,7 +2,7 @@ import firestore from '@react-native-firebase/firestore';
 
 export const setFeedback = async (data, user) => {
   const {Message, Rating, Type} = data;
-  const {displayName, email} = user._user;
+  const {displayName, email} = user;
   const result = await firestore().collection('Feedback').add({
     DisplayName: displayName,
     Email: email,
